917cd472b43a13d40e103cd00f1719cabba5a49b
Kutay Gunal <kutaygunal@outlook.com>
2026-03-04 07:53:10 -0500

    Initial Commit

---
+++ PostureAI/PostureAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostureAIApp.swift
+//  PostureAI
+//
+//  Created by Kutay Gunal on 3/4/26.
+//
+
+import SwiftUI
+
+@main
+struct PostureAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
